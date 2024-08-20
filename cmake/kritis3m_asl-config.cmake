38cc4cabd738b6ddf0f5ad3c8a99e0b7fa276700
Tobias Frauenschläger <tobias.frauenschlaeger@oth-regensburg.de>
2024-08-20 13:04:37 +0200

    More CMake fixes

--- cmake/kritis3m_asl-config.cmake
+++ cmake/kritis3m_asl-config.cmake
@@ -1,2 +1,9 @@
+include(CMakeFindDependencyMacro)
+
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+
+find_dependency(Threads)
+find_dependency(liboqs)
+find_dependency(wolfssl)
+
 include(${SELF_DIR}/kritis3m_asl-export.cmake)
